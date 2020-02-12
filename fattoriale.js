444c171604580a604d7d6bccb30186b5d9238832
MSale111 <martina.sale.111@gmail.com>
2020-02-12 17:02:38 +0100

    Fattoriale, non ho ben capito cosa devo scrivere nel messaggio

--- fattoriale.js
+++ fattoriale.js
@@ -3,8 +3,8 @@
 **/
 function fattoriale (n){
     if (n===1){
-        return 1
+        return 1;
     }
-return n*(fattoriale(n-1))
+return n*(fattoriale(n-1));
 }
-console.log(fattoriale(5))
+console.log(fattoriale(5));
