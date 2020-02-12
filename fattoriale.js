9194af81923cca12a47965d27a097b162100d041
MSale111 <martina.sale.111@gmail.com>
2020-02-12 17:00:47 +0100

    Fattoriale

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,10 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
+function fattoriale (n){
+    if (n===1){
+        return 1
+    }
+return n*(fattoriale(n-1))
+}
+console.log(fattoriale(5))
